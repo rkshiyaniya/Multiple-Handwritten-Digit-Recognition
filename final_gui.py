891edb73835073ea9c4bfced1cfce8c5c480b29b
Rutvik Shiyaniya <52591725+rkshiyaniya@users.noreply.github.com>
2020-07-02 15:09:41 +0530

    Update final_gui.py

--- final_gui.py
+++ final_gui.py
@@ -42,8 +42,6 @@ cv.bind('<Button-1>', activate_event)
 
 def Recognize_Digit():
     global image_number
-    predictions = []
-    percentage = []
     filename = f'img_{image_number}.png'
     widget = cv
 
@@ -53,6 +51,7 @@ def Recognize_Digit():
     y1 = y + widget.winfo_height()
     print(x, y, x1, y1)
 
+    # get image and save
     ImageGrab.grab().crop((x, y, x1, y1)).save(image_folder + filename)
 
     image = cv2.imread(image_folder + filename, cv2.IMREAD_COLOR)
@@ -60,10 +59,10 @@ def Recognize_Digit():
     ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     contours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    preprocessed_digits = []
 
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
+        # make a rectangle box around each curve
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
         # Cropping out the digit from the image corresponding to the current contours in the for loop
@@ -95,9 +94,7 @@ def Recognize_Digit():
 
 btn_save = Button(text='Recognize Digit', command=Recognize_Digit)
 btn_save.grid(row=2, column=0, pady=1, padx=1)
-# btn_save.bind('<Button-1>', Recognize_Digit)
 button_clear = Button(text='Clear Widget', command=clear_widget)
 button_clear.grid(row=2, column=1, pady=1, padx=1)
-# btn_save.bind('<Button-1>', clear_widget)
 
 root.mainloop()
